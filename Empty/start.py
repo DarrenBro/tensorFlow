e3c7847fa78cd5b36af7484b974a44b5e791ba52
Darren Broderick <darren.broderick@liberty-it.co.uk>
2020-06-03 20:26:24 +0100

    final files

--- Empty/start.py
+++ Empty/start.py
@@ -1,11 +1,13 @@
-import tensorflow as tf
+# X = -1, 0, 1, 2, 3, 4
+# Y = -3, -1, 1, 3, 5, 7
+
+# y = 2x - 1
+
 import numpy as np
 from tensorflow import keras
 
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
-
-sgd = tf.optimizers.SGD(lr=0.01)
 model.compile(loss='mean_squared_error', optimizer='sgd')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
@@ -13,4 +15,8 @@ ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 model.fit(xs, ys, epochs=500)
 
+# predict y when x = 10
+# 19
+
 print(model.predict([10.0]))
+
